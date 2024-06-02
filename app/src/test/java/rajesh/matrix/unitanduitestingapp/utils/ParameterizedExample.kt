86a2fc5a63c4b92198febdf29efa022778e39327
RajeshKumar <rajeshincorp@gmail.com>
2024-06-03 00:20:32 +0530

    Parameterised Unit Test example

---
+++ app/src/test/java/rajesh/matrix/unitanduitestingapp/utils/ParameterizedExample.kt
@@ -0,0 +1,30 @@
+package rajesh.matrix.unitanduitestingapp.utils
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(value = Parameterized::class)
+class ParameterizedExample(val input: String, val expectedValue: Boolean){
+
+    @Test
+    fun test(){
+        val helper = Helper()
+        val result = helper.isPalindrome(input)
+        assertEquals(expectedValue,result)
+    }
+
+    companion object{
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0} is palindrome - {1}")
+        fun data() : List<Array<Any>>{
+            return listOf(
+                arrayOf("Hello",false),
+                arrayOf("malayalam",true),
+                arrayOf("madam",true),
+                arrayOf("Super",false)
+            )
+        }
+    }
+}
