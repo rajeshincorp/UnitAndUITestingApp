8aa2643698b41f1be7ff94c673edf6a678fd8733
RajeshKumar <rajeshincorp@gmail.com>
2024-06-02 23:39:50 +0530

    unit test @Before and @After annotation

--- app/src/test/java/rajesh/matrix/unitanduitestingapp/utils/HelperTest.kt
+++ app/src/test/java/rajesh/matrix/unitanduitestingapp/utils/HelperTest.kt
@@ -1,14 +1,29 @@
 package rajesh.matrix.unitanduitestingapp.utils
 
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 class HelperTest {
 
+    lateinit var helper: Helper
+
+    @Before
+    fun setUp(){
+        helper = Helper()
+        println("Before every test")
+    }
+
+    @After
+    fun tearD(){
+        println("After every test")
+    }
+
     @Test
     fun isPalindrome_Hello() {
         //Arrange
-        val helper = Helper()
+          /*val helper = Helper()*/
         //Act
         val result = helper.isPalindrome("Hello")
         //Assert
@@ -18,7 +33,7 @@ class HelperTest {
     @Test
     fun isPalindrome_malayalam() {
         //Arrange
-        val helper = Helper()
+          /*val helper = Helper()*/
         //Act
         val result = helper.isPalindrome("malayalam")
         //Assert
